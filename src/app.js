114a85513fb2a6b82d1877ad7ad6cd7080cd9521
kickdirt <ziggy--stardust--@hotmail.com>
2021-04-13 20:43:54 +0100

    Added HTML for forecast into JS
    Something isn't working

--- src/app.js
+++ src/app.js
@@ -24,6 +24,43 @@
   return `Last updated ${day},${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 let dateElement = document.querySelector("h3");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
@@ -58,7 +95,6 @@ dateElement.innerHTML = formatDate(currentTime);
            iconElement.setAttribute("alt", response.data.weather[0].description);
         }
 
-        
  
 
             
