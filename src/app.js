71bd728d8a62e560a73e66640b1548523f15ba49
kickdirt <ziggy--stardust--@hotmail.com>
2021-04-14 20:33:45 +0100

    Added working forecast

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = ["S", "M", "T", "W", "T", "F", "S"];
 
   return days[day];
 }
@@ -61,6 +61,14 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 let dateElement = document.querySelector("h3");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
@@ -84,19 +92,22 @@ dateElement.innerHTML = formatDate(currentTime);
 
     function weatherToday(response) {
             let iconElement = document.querySelector("#icon");
-            
+
             document.querySelector("h1").innerHTML = response.data.name;
             document.querySelector("#todays-temp").innerHTML = Math.round(response.data.main.temp) + "°C";
             document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed) + " km/h";
             document.querySelector("#description").innerHTML = (response.data.weather[0].main);
             document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity) + "%";
-            
+           
            iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
            iconElement.setAttribute("alt", response.data.weather[0].description);
+
+           getForecast(response.data.coord);
         }
 
- 
+        
 
+        console.log(response.data.daily);
             
 
 
